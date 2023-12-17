209c009de6d889efd3ce8622aada766462d8406c
Posyolova <143779118+Posyolova@users.noreply.github.com>
2023-12-17 19:05:02 +0700

    Update gener.py

--- gener.py
+++ gener.py
@@ -3,5 +3,5 @@ q = [
     {'id': 2, 'full_name': 'Четверикова Олеся Александровна'},
     {'id': 3, 'full_name': 'Бобровская Анастасия Дмитриевна'}
     ]
-w = [q['full_name'] for q in q if len(q['full_name'].split()[1]) > 6]
+w = [q['full_name'] for q in q if len(q['full_name'].split()[1]) > 6] # генератор списка для фильтрации студентов
 print(w)
